fcb5e4224e88686c52bf90f284cbd355691c3afa
René van Dorst <opensource@vdorst.com>
2023-10-15 22:37:44 +0200

    modbus: refactor more modbus code. Better state handeling

--- modbus_master/src/lib.rs
+++ modbus_master/src/lib.rs
@@ -9,39 +9,53 @@ use core::ops::RangeInclusive;
 
 /// Minimal Modbus Frame Length `<ADDR> <FUNC+0x80> <ERRORCODE> <CRC_HIGH> <CRC_LOW>`
 const MODBUS_FRAME_LEN_MIN: u8 = 5;
-/// Maximum Modbus RTU `FrameLength`.
-pub const MODBUS_FRAME_LEN_MAX: u8 = 255;
 
 /// Valid Modbus address range
-const MODBUS_SLAVE_ADDR_RANGE: RangeInclusive<u8> = 1..=247;
+const MODBUS_ADDR_RANGE: RangeInclusive<u8> = 1..=247;
+/// Modbus broadcast address
+const MODBUS_ADDR_BROADCAST: u8 = 0;
 
-#[derive(Debug, Default, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-pub enum ModbusStateError {
-    #[default]
-    None,
+pub enum ModbusError {
+    /// Our address don´t match
     InvalidAddr,
-    MoreDataAtferT35,
-    SerialError,
+    /// Received some data in t3.5 wait time.
+    MoreDataWithinT35Wait,
+    /// Received more data then our buffer
     TooLong,
+    /// Received too less data
     TooShort,
+    /// Crc don´t match
     Crc,
+    /// Serial Parity error
+    Parity,
+    /// Serial OverRun error
+    OverRun,
+    /// Serial Frame error
+    Frame,
 }
 
-#[derive(Debug, Default, PartialEq, Eq, Clone, Copy)]
+type MbTmrResult = Result<NonZeroU8, ModbusError>;
+
+#[derive(Debug, Default, PartialEq, Clone, Copy)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum ModbusState {
+    /// Idle, waiting for a new frame
     #[default]
     Idle,
-    InTrans(ModbusStateError),
-    WaitForT3_5(ModbusStateError),
+    /// Receiving a frame in progress
+    Reception(u8),
+    /// Receiving a frame in with error or not ours
+    Emission(ModbusError),
+    /// Waiting for t3.5 to expire
+    WaitForT3_5(MbTmrResult),
 }
 
 pub struct Modbus<const N: usize> {
     pub buf: [u8; N],
-    idx: u8,
-    addr: ModbusAddr,
     pub state: ModbusState,
+    addr: ModbusAddr,
     // tmr: Capture<TIM9, 10000>,
     //queue: Producer<'static, Box<SERMB>, 4>,
 }
@@ -67,7 +81,7 @@ pub struct ModbusAddr(Option<NonZeroU8>);
 impl ModbusAddr {
     /// Validates modbus address
     pub fn new(addr: Option<u8>) -> Option<Self> {
-        if addr.is_some_and(|addr| !MODBUS_SLAVE_ADDR_RANGE.contains(&(addr))) {
+        if addr.is_some_and(|addr| !MODBUS_ADDR_RANGE.contains(&(addr))) {
             return None;
         }
         let addr = addr.and_then(NonZeroU8::new);
@@ -75,40 +89,13 @@ impl ModbusAddr {
     }
 }
 
-#[derive(Debug, Default, PartialEq, Eq)]
-#[cfg_attr(feature = "defmt", derive(defmt::Format))]
-pub enum ModbusError {
-    #[default]
-    None,
-    Nok,
-    Crc,
-}
-
 impl<const N: usize> Modbus<N> {
     #[must_use]
     pub fn new(addr: ModbusAddr) -> Self {
         Self {
             buf: [0; N],
-            idx: 0,
             addr,
             state: ModbusState::default(),
-        }
-    }
-
-    #[must_use]
-    pub fn idx(&self) -> usize {
-        usize::from(self.idx)
-    }
-
-    #[allow(clippy::result_unit_err)]
-    pub fn push(&mut self, byte: u8) -> Result<(), ()> {
-        let idx = self.idx();
-        if idx == self.buf.len() {
-            Err(())
-        } else {
-            self.buf[idx] = byte;
-            self.idx += 1;
-            Ok(())
         }
     }
 
@@ -118,83 +105,100 @@ impl<const N: usize> Modbus<N> {
     }
 
     pub fn reset(&mut self) {
-        self.idx = 0;
         self.state = ModbusState::default();
     }
 
+    /// Gives address our own or broadcast.
+    fn accept_address(&self, address: u8) -> bool {
+        self.addr.0.is_some_and(|addr| u8::from(addr) == address)
+            || self.addr.0.is_none()
+            || address == MODBUS_ADDR_BROADCAST
+    }
+
     /// Should be called by the UART RX interrupt
-    pub fn char_recv(&mut self, data: u8) {
-        match &self.state {
+    pub fn serial_recv(&mut self, data: u8) {
+        match &mut self.state {
             ModbusState::Idle => {
-                self.state = if self.addr.0.is_none()
-                    || self.addr.0.is_some_and(|addr| u8::from(addr) == data)
-                    || data == 0
-                {
-                    // Clear the buffer.
-                    self.idx = 0;
-                    let _ = self.push(data);
-                    ModbusState::InTrans(ModbusStateError::None)
+                self.state = if self.accept_address(data) {
+                    self.buf[0] = data;
+                    ModbusState::Reception(0)
                 } else {
-                    ModbusState::InTrans(ModbusStateError::InvalidAddr)
+                    ModbusState::Emission(ModbusError::InvalidAddr)
                 };
             }
-            ModbusState::InTrans(err) => {
-                if matches!(err, ModbusStateError::None) && self.push(data).is_err() {
-                    self.state = ModbusState::InTrans(ModbusStateError::TooLong);
+            ModbusState::Reception(idx) => {
+                if usize::from(*idx) < (N - 1) {
+                    *idx += 1;
+                    self.buf[usize::from(*idx)] = data;
+                } else {
+                    self.state = ModbusState::Emission(ModbusError::TooLong);
                 }
             }
+            ModbusState::Emission(_) => (),
             ModbusState::WaitForT3_5(_) => {
-                self.state = ModbusState::InTrans(ModbusStateError::MoreDataAtferT35);
+                self.state = ModbusState::Emission(ModbusError::MoreDataWithinT35Wait);
             }
         }
     }
 
-    /// Should be called bij de t1.5/t3.5 timer interrupt
-    pub fn timer_handle(&mut self) -> Result<u8, ModbusStateError> {
+    /// Should be called by the UART RX interrupt incase of an serial error
+    /// Error is ignored when `Idle` or there is already an error or `WaitForT3_5`.
+    pub fn serial_error(&mut self, mbs_err: ModbusError) {
+        match self.state {
+            ModbusState::Reception(_) => self.state = ModbusState::Emission(mbs_err),
+            ModbusState::Idle | ModbusState::Emission(_) | ModbusState::WaitForT3_5(_) => (),
+        }
+    }
+
+    /// Should be called by the t1.5/t3.5 timer
+    pub fn timer_handle(&mut self) -> Option<MbTmrResult> {
         match self.state {
             ModbusState::WaitForT3_5(mbs_err) => {
                 // Go back to Idle
                 self.state = ModbusState::Idle;
 
-                // self.tmr.disable_timer();
-                if matches!(mbs_err, ModbusStateError::None) {
-                    if self.idx >= MODBUS_FRAME_LEN_MIN {
-                        let crc_start = self.idx() - 2;
-                        let crc_data = &self.buf[0..crc_start];
-                        let crc_calc = crc16(crc_data).to_le_bytes();
-                        let crc_calc = crc_calc.as_slice();
-                        let crc_recv = &self.buf[crc_start..self.idx()];
-                        if crc_calc == crc_recv {
-                            // if let Some(buf) = SERMB::alloc() {
-                            //     let mut d = buf.init(Vec::new());
-                            //     d.clone_from(&self.buf);
-                            //     let _ = self.queue.enqueue(d);
-                            // }
-                            #[cfg(feature = "std")]
-                            println!("Got valid data D: {:02x?}", crc_data);
-                            Ok(u8::try_from(crc_start & 0xFF).unwrap())
-                        } else {
-                            // #[cfg(feature = "defmt")]
-                            // defmt::println!("CRC C: {:X} R {:X} D: {:X}", crc_calc, crc_recv, crc_data);
-                            #[cfg(feature = "std")]
-                            println!(
-                                "CRC C: {:02x?} R {:02x?} D: {:02x?}",
-                                crc_calc, crc_recv, crc_data
-                            );
-                            Err(ModbusStateError::Crc)
-                        }
+                Some(mbs_err)
+            }
+            ModbusState::Reception(idx) => {
+                // To fit idx in a u8 the address is not counted!
+                // first byte of the buffer is filled once state is changed to `Reception`;
+                let idx = usize::from(idx) + 1;
+                let mbs_err = if idx >= usize::from(MODBUS_FRAME_LEN_MIN) {
+                    let crc_start = idx - 2;
+                    let crc_data = &self.buf[0..crc_start];
+                    let crc_calc = crc16(crc_data).to_le_bytes();
+                    let crc_calc = crc_calc.as_slice();
+                    let crc_recv = &self.buf[crc_start..idx];
+                    if crc_calc == crc_recv {
+                        // if let Some(buf) = SERMB::alloc() {
+                        //     let mut d = buf.init(Vec::new());
+                        //     d.clone_from(&self.buf);
+                        //     let _ = self.queue.enqueue(d);
+                        // }
+                        #[cfg(feature = "std")]
+                        println!("Got valid data D: {:02x?}", crc_data);
+                        Ok(NonZeroU8::try_from(u8::try_from(crc_start & 0xFF).unwrap()).unwrap())
                     } else {
-                        Err(ModbusStateError::TooShort)
+                        // #[cfg(feature = "defmt")]
+                        // defmt::println!("CRC C: {:X} R {:X} D: {:X}", crc_calc, crc_recv, crc_data);
+                        #[cfg(feature = "std")]
+                        println!(
+                            "CRC C: {:02x?} R {:02x?} D: {:02x?}",
+                            crc_calc, crc_recv, crc_data
+                        );
+                        Err(ModbusError::Crc)
                     }
                 } else {
-                    Err(mbs_err)
-                }
-            }
-            ModbusState::InTrans(mbs_err) => {
+                    Err(ModbusError::TooShort)
+                };
                 self.state = ModbusState::WaitForT3_5(mbs_err);
-                Err(mbs_err)
+                None
             }
-            ModbusState::Idle => Err(ModbusStateError::None),
+            ModbusState::Emission(mbs_err) => {
+                self.state = ModbusState::WaitForT3_5(Err(mbs_err));
+                None
+            }
+            ModbusState::Idle => None,
         }
     }
 }
@@ -216,9 +220,7 @@ pub fn create_request(buf: &mut [u8], addr: u8, reg: u16, len: u8) -> Option<Non
 
 #[cfg(test)]
 mod test {
-    use super::{
-        create_request, Modbus, ModbusAddr, ModbusState, ModbusStateError, MODBUS_FRAME_LEN_MAX,
-    };
+    use super::{create_request, Modbus, ModbusAddr, ModbusError, ModbusState};
     use core::num::NonZeroU8;
 
     #[test]
@@ -249,7 +251,7 @@ mod test {
         b"\x01\x03\x06\x00\x00\x00\x00\x00\x03\x61\x74",
     ];
 
-    const VALID_TEST_LONG: &[u8; MODBUS_FRAME_LEN_MAX as usize] = &[
+    const VALID_TEST_LONG: &[u8; 256] = &[
         0x01, 0x03, 252, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -266,7 +268,8 @@ mod test {
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x17, 0xae,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xee,
+        0x0e,
     ];
 
     #[test]
@@ -276,21 +279,20 @@ mod test {
         // Valid
         for data in VALID_TEST_SET {
             for &byte in data {
-                mb.char_recv(byte);
+                mb.serial_recv(byte);
             }
 
-            assert_eq!(mb.state, ModbusState::InTrans(ModbusStateError::None));
-            assert_eq!(mb.idx(), data.len());
-
-            assert_eq!(mb.timer_handle(), Err(ModbusStateError::None));
-            assert_eq!(mb.state, ModbusState::WaitForT3_5(ModbusStateError::None));
-
-            assert_eq!(mb.idx(), data.len());
+            let idx_data_without_crc =
+                NonZeroU8::try_from(u8::try_from(data.len() - 2).unwrap()).unwrap();
 
             assert_eq!(
-                mb.timer_handle(),
-                Ok(u8::try_from(data.len() & 0xFF).unwrap() - 2)
+                mb.state,
+                ModbusState::Reception(u8::from(idx_data_without_crc) + 1)
             );
+            assert_eq!(mb.timer_handle(), None);
+            assert_eq!(mb.state, ModbusState::WaitForT3_5(Ok(idx_data_without_crc)));
+
+            assert_eq!(mb.timer_handle(), Some(Ok(idx_data_without_crc)));
             assert_eq!(mb.state, ModbusState::Idle);
         }
     }
@@ -302,16 +304,16 @@ mod test {
         // Valid
         for data in VALID_TEST_SET {
             for &byte in &data[0..data.len() - 1] {
-                mb.char_recv(byte);
+                mb.serial_recv(byte);
             }
-            mb.char_recv(0xFF);
+            mb.serial_recv(0xF0);
 
-            assert_eq!(mb.state, ModbusState::InTrans(ModbusStateError::None));
+            let idx: u8 = u8::try_from(data.len() - 1).unwrap();
+            assert_eq!(mb.state, ModbusState::Reception(idx));
+            assert_eq!(mb.timer_handle(), None);
 
-            assert_eq!(mb.timer_handle(), Err(ModbusStateError::None));
-            assert_eq!(mb.state, ModbusState::WaitForT3_5(ModbusStateError::None));
-
-            assert_eq!(mb.timer_handle(), Err(ModbusStateError::Crc));
+            assert_eq!(mb.state, ModbusState::WaitForT3_5(Err(ModbusError::Crc)));
+            assert_eq!(mb.timer_handle(), Some(Err(ModbusError::Crc)));
             assert_eq!(mb.state, ModbusState::Idle);
         }
     }
@@ -329,24 +331,21 @@ mod test {
 
     #[test]
     fn packet_too_long() {
-        let mut mb = Modbus::<37>::new(ModbusAddr(NonZeroU8::new(0x01)));
+        let mut mb = Modbus::<256>::new(ModbusAddr(NonZeroU8::new(0x01)));
 
         for &byte in VALID_TEST_LONG {
-            mb.char_recv(byte);
+            mb.serial_recv(byte);
         }
+        mb.serial_recv(0xFF);
 
+        assert_eq!(mb.state, ModbusState::Emission(crate::ModbusError::TooLong));
+        assert_eq!(mb.timer_handle(), None);
         assert_eq!(
             mb.state,
-            ModbusState::InTrans(crate::ModbusStateError::TooLong)
+            ModbusState::WaitForT3_5(Err(ModbusError::TooLong))
         );
 
-        assert_eq!(mb.timer_handle(), Err(ModbusStateError::TooLong));
-        assert_eq!(
-            mb.state,
-            ModbusState::WaitForT3_5(ModbusStateError::TooLong)
-        );
-
-        assert_eq!(mb.timer_handle(), Err(ModbusStateError::TooLong));
+        assert_eq!(mb.timer_handle(), Some(Err(ModbusError::TooLong)));
         assert_eq!(mb.state, ModbusState::Idle);
     }
 
@@ -354,19 +353,20 @@ mod test {
     fn packet_too_short() {
         let mut mb = Modbus::<32>::new(ModbusAddr(NonZeroU8::new(0x01)));
 
+        assert_eq!(mb.state, ModbusState::Idle);
+
         for &byte in &VALID_TEST_LONG[0..4] {
-            mb.char_recv(byte);
+            mb.serial_recv(byte);
         }
 
+        assert_eq!(mb.state, ModbusState::Reception(3));
+
+        assert_eq!(mb.timer_handle(), None);
         assert_eq!(
             mb.state,
-            ModbusState::InTrans(crate::ModbusStateError::None)
+            ModbusState::WaitForT3_5(Err(ModbusError::TooShort))
         );
-
-        assert_eq!(mb.timer_handle(), Err(ModbusStateError::None));
-        assert_eq!(mb.state, ModbusState::WaitForT3_5(ModbusStateError::None));
-
-        assert_eq!(mb.timer_handle(), Err(ModbusStateError::TooShort));
+        assert_eq!(mb.timer_handle(), Some(Err(ModbusError::TooShort)));
         assert_eq!(mb.state, ModbusState::Idle);
     }
 
@@ -375,18 +375,38 @@ mod test {
         let mut mb = Modbus::<256>::new(ModbusAddr(NonZeroU8::new(0x01)));
 
         for &byte in VALID_TEST_LONG {
-            mb.char_recv(byte);
+            mb.serial_recv(byte);
         }
+
+        assert_eq!(mb.state, ModbusState::Reception(u8::MAX));
+        assert_eq!(mb.timer_handle(), None);
 
         assert_eq!(
             mb.state,
-            ModbusState::InTrans(crate::ModbusStateError::None)
+            ModbusState::WaitForT3_5(Ok(NonZeroU8::new(254).unwrap()))
         );
+        assert_eq!(mb.timer_handle(), Some(Ok(NonZeroU8::new(254).unwrap())));
 
-        assert_eq!(mb.timer_handle(), Err(ModbusStateError::None));
-        assert_eq!(mb.state, ModbusState::WaitForT3_5(ModbusStateError::None));
+        assert_eq!(mb.state, ModbusState::Idle);
+    }
 
-        assert_eq!(mb.timer_handle(), Ok(VALID_TEST_LONG.len() as u8 - 2));
+    #[test]
+    fn packet_valid_serial_error() {
+        let mut mb = Modbus::<256>::new(ModbusAddr(NonZeroU8::new(0x01)));
+
+        for &byte in VALID_TEST_LONG {
+            mb.serial_recv(byte);
+        }
+
+        assert_eq!(mb.state, ModbusState::Reception(u8::MAX));
+
+        mb.serial_error(ModbusError::Parity);
+
+        assert_eq!(mb.timer_handle(), None);
+
+        assert_eq!(mb.state, ModbusState::WaitForT3_5(Err(ModbusError::Parity)));
+        assert_eq!(mb.timer_handle(), Some(Err(ModbusError::Parity)));
+
         assert_eq!(mb.state, ModbusState::Idle);
     }
 }
